a67b0e6d97a48a20570312709a4e3dd1fe5a55e2
williamyadi <william.yadi@tokopedia.com>
2024-04-30 19:22:44 +0700

    update sources script

--- deploy-sources.sh
+++ deploy-sources.sh
@@ -12,7 +12,7 @@ curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" 
     "database.user": "yugabyte",
     "database.password":"yugabyte",
     "database.dbname":"yugabyte",
-    "database.server.name":"ybconnector1",
+    "database.server.name":"postgres",
     "snapshot.mode":"initial",
     "database.streamid":"'$1'",
     "table.include.list":"public.orders[a-zA-Z0-9]*",
@@ -24,87 +24,87 @@ curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" 
     "value.converter.schema.registry.url":"http://schema-registry:8081",
     "value.converter.enhanced.avro.schema.support":"true"
   }
-}'
+  }'
 
-sleep 1;
+  sleep 1;
 
-# Deploy connector 2
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d '{
-  "name": "ybconnector2",
-  "config": {
-    "tasks.max":"2",
-    "connector.class": "io.debezium.connector.yugabytedb.YugabyteDBConnector",
-    "database.hostname":"'$NODE'",
-    "database.master.addresses":"'$MASTERS'",
-    "database.port":"5433",
-    "database.user": "yugabyte",
-    "database.password":"yugabyte",
-    "database.dbname":"yugabyte",
-    "database.server.name":"ybconnector2",
-    "snapshot.mode":"initial",
-    "database.streamid":"'$1'",
-    "table.include.list":"public.products",
-    "key.converter":"io.confluent.connect.avro.AvroConverter",
-    "key.converter.schema.registry.url":"http://schema-registry:8081",
-    "key.converter.enhanced.avro.schema.support":"true",
-    "value.converter":"io.confluent.connect.avro.AvroConverter",
-    "value.converter.schema.registry.url":"http://schema-registry:8081",
-    "value.converter.enhanced.avro.schema.support":"true"
-  }
-}'
+  # Deploy connector 2
+  curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d '{
+    "name": "ybconnector2",
+    "config": {
+      "tasks.max":"2",
+      "connector.class": "io.debezium.connector.yugabytedb.YugabyteDBConnector",
+      "database.hostname":"'$NODE'",
+      "database.master.addresses":"'$MASTERS'",
+      "database.port":"5433",
+      "database.user": "yugabyte",
+      "database.password":"yugabyte",
+      "database.dbname":"yugabyte",
+      "database.server.name":"postgres",
+      "snapshot.mode":"initial",
+      "database.streamid":"'$1'",
+      "table.include.list":"public.products",
+      "key.converter":"io.confluent.connect.avro.AvroConverter",
+      "key.converter.schema.registry.url":"http://schema-registry:8081",
+      "key.converter.enhanced.avro.schema.support":"true",
+      "value.converter":"io.confluent.connect.avro.AvroConverter",
+      "value.converter.schema.registry.url":"http://schema-registry:8081",
+      "value.converter.enhanced.avro.schema.support":"true"
+    }
+  }'
 
-sleep 1;
+  sleep 1;
 
-# Deploy connector 3
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d '{
-  "name": "ybconnector3",
-  "config": {
-    "tasks.max":"2",
-    "connector.class": "io.debezium.connector.yugabytedb.YugabyteDBConnector",
-    "database.hostname":"'$NODE'",
-    "database.master.addresses":"'$MASTERS'",
-    "database.port":"5433",
-    "database.user": "yugabyte",
-    "database.password":"yugabyte",
-    "database.dbname":"yugabyte",
-    "database.server.name":"ybconnector3",
-    "snapshot.mode":"initial",
-    "database.streamid":"'$1'",
-    "table.include.list":"public.users",
-    "key.converter":"io.confluent.connect.avro.AvroConverter",
-    "key.converter.schema.registry.url":"http://schema-registry:8081",
-    "key.converter.enhanced.avro.schema.support":"true",
-    "value.converter":"io.confluent.connect.avro.AvroConverter",
-    "value.converter.schema.registry.url":"http://schema-registry:8081",
-    "value.converter.enhanced.avro.schema.support":"true"
-  }
-}'
+  # Deploy connector 3
+  curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d '{
+    "name": "ybconnector3",
+    "config": {
+      "tasks.max":"2",
+      "connector.class": "io.debezium.connector.yugabytedb.YugabyteDBConnector",
+      "database.hostname":"'$NODE'",
+      "database.master.addresses":"'$MASTERS'",
+      "database.port":"5433",
+      "database.user": "yugabyte",
+      "database.password":"yugabyte",
+      "database.dbname":"yugabyte",
+      "database.server.name":"postgres",
+      "snapshot.mode":"initial",
+      "database.streamid":"'$1'",
+      "table.include.list":"public.users",
+      "key.converter":"io.confluent.connect.avro.AvroConverter",
+      "key.converter.schema.registry.url":"http://schema-registry:8081",
+      "key.converter.enhanced.avro.schema.support":"true",
+      "value.converter":"io.confluent.connect.avro.AvroConverter",
+      "value.converter.schema.registry.url":"http://schema-registry:8081",
+      "value.converter.enhanced.avro.schema.support":"true"
+    }
+  }'
 
-sleep 1;
+  sleep 1;
 
-# Deploy connector 4
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d '{
-  "name": "ybconnector4",
-  "config": {
-    "tasks.max":"2",
-    "connector.class": "io.debezium.connector.yugabytedb.YugabyteDBConnector",
-    "database.hostname":"'$NODE'",
-    "database.master.addresses":"'$MASTERS'",
-    "database.port":"5433",
-    "database.user": "yugabyte",
-    "database.password":"yugabyte",
-    "database.dbname":"yugabyte",
-    "database.server.name":"ybconnector4",
-    "snapshot.mode":"initial",
-    "database.streamid":"'$1'",
-    "table.include.list":"public.reviews",
-    "key.converter":"io.confluent.connect.avro.AvroConverter",
-    "key.converter.schema.registry.url":"http://schema-registry:8081",
-    "key.converter.enhanced.avro.schema.support":"true",
-    "value.converter":"io.confluent.connect.avro.AvroConverter",
-    "value.converter.schema.registry.url":"http://schema-registry:8081",
-    "value.converter.enhanced.avro.schema.support":"true"
-  }
-}'
+  # Deploy connector 4
+  curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d '{
+    "name": "ybconnector4",
+    "config": {
+      "tasks.max":"2",
+      "connector.class": "io.debezium.connector.yugabytedb.YugabyteDBConnector",
+      "database.hostname":"'$NODE'",
+      "database.master.addresses":"'$MASTERS'",
+      "database.port":"5433",
+      "database.user": "yugabyte",
+      "database.password":"yugabyte",
+      "database.dbname":"yugabyte",
+      "database.server.name":"postgres",
+      "snapshot.mode":"initial",
+      "database.streamid":"'$1'",
+      "table.include.list":"public.reviews",
+      "key.converter":"io.confluent.connect.avro.AvroConverter",
+      "key.converter.schema.registry.url":"http://schema-registry:8081",
+      "key.converter.enhanced.avro.schema.support":"true",
+      "value.converter":"io.confluent.connect.avro.AvroConverter",
+      "value.converter.schema.registry.url":"http://schema-registry:8081",
+      "value.converter.enhanced.avro.schema.support":"true"
+    }
+  }'
 
-sleep 1;
+  sleep 1;
